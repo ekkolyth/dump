7e9753eb2947261459a15d4c481d4c1961a13e48
Mike Kenway <mike@mikekenway.com>
2026-04-04 17:55:38 -0400

    feat: add subcommand routing (version, upgrade, resume)

--- cmd/dump/main.go
+++ cmd/dump/main.go
@@ -1,29 +1,52 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/ekkolyth/dump/internal/tui"
+	"github.com/ekkolyth/dump/internal/version"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	resumeID := flag.String("resume", "", "Resume a previous session by ID")
-	flag.Parse()
-
-	var m tea.Model
-	if *resumeID != "" {
-		m = tui.ResumeModel(*resumeID)
-	} else {
-		m = tui.InitialModel()
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "version":
+			fmt.Printf("dump v%s\n", version.Version)
+			return
+		case "upgrade":
+			if err := runUpgrade(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+			return
+		case "resume":
+			if len(os.Args) < 3 {
+				fmt.Fprintln(os.Stderr, "Usage: dump resume <session-id>")
+				os.Exit(1)
+			}
+			runTUI(tui.ResumeModel(os.Args[2]))
+			return
+		default:
+			fmt.Fprintf(os.Stderr, "Unknown command: %s\nUsage: dump [version|upgrade|resume <id>]\n", os.Args[1])
+			os.Exit(1)
+		}
 	}
 
+	runTUI(tui.InitialModel())
+}
+
+func runTUI(m tea.Model) {
 	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func runUpgrade() error {
+	fmt.Println("Upgrade not yet implemented")
+	return nil
 }
